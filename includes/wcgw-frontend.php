42266aafa2768e8bffa9727610e9d6771de50682
Koen Van den Wijngaert <koen@koenvandenwijngaert.be>
2014-09-30 17:02:06 +0200

    Frontend gift wrapping.

--- includes/wcgw-frontend.php
+++ includes/wcgw-frontend.php
@@ -4,13 +4,44 @@ class WCGW_Frontend {
 
 	public function __construct() {
 		add_filter('woocommerce_cart_item_name', array($this, 'add_to_cart'), 1, 3);
+		add_filter('woocommerce_update_cart_action_cart_updated', array($this, 'cart_updated'));
 	}
 
-	public function add_to_cart($product_name, $product, $b) {
-		$select = '<br><label><strong>' . _x('Wrapping', 'In cart', 'woocommerce-gift-wrap') . ': </strong>';
-		$select .= '<select name="wrapping[' . $product['product_id'] . ']"><option value="">' . _x('Wrapping', 'in selectbox', 'woocommerce-gift-wrap') . '</option>';
+	public function cart_updated($cart_updated) {
 
-		// new options
+
+		$wrappings = array();
+		$options = self::get_options();
+		foreach($_POST['cart'] as $item_key => $cart_item) {
+			if(isset($cart_item['wrapping']) && array_key_exists($cart_item['wrapping'], $options)) {
+				$wrappings[$item_key] = $cart_item['wrapping'];
+			}
+		}
+
+		$_SESSION['wcgw_wrappings'] = $wrappings;
+
+		return $cart_updated;
+	}
+
+	public function add_to_cart($product_name, $product, $id) {
+		$select = '<br><label><strong>' . _x('Wrapping', 'In cart', 'woocommerce-gift-wrap') . ': </strong>';
+		$select .= '<select name="cart[' . $id . '][wrapping]">';
+
+		$selected = 'none';
+
+		if(isset($_SESSION, $_SESSION['wcgw_wrappings'], $_SESSION['wcgw_wrappings'][$id])) {
+			$selected = $_SESSION['wcgw_wrappings'][$id];
+		}
+
+
+		$options = self::get_options();
+		foreach($options as $k => $v) {
+
+			$select .= '<option value="' . $k . '"' . selected($k, $selected, false) . '>' . $v . '</option>';
+		}
+
+
+
 
 		$select .= '</select></label>';
 
@@ -23,7 +54,7 @@ class WCGW_Frontend {
 
 		$return = array();
 
-		$return['empty'] = __( '-- Choose an option --', 'woocommerce-gift-wrap' );
+		$return['none'] = _x( 'None', 'No wrapping in selectbox', 'woocommerce-gift-wrap' );
 
 		foreach ( $options as $option ) {
 			$return[ self::slugify( $option ) ] = $option;
